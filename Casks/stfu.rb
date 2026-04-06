d03ff11d5fc070c6990eaf6374631b0b532b26c8
Slavik Sendel <s.sendel@ext.douglas.eu>
2026-04-06 14:44:28 +0300

    updated stfu app with dmg file

--- Casks/stfu.rb
+++ Casks/stfu.rb
@@ -1,6 +1,6 @@
 cask "stfu" do
   version "2026.04.01"
-  sha256 "947ba03900a29d293741d99dac7422774651284625beb2e40a14e0a62601b60a"
+  sha256 "f3636de1bebb63e5d7558b3ec0b79f7531039659983806c87399ce620bfe57fa"
 
   url "https://github.com/J3ys/sendel/releases/download/v#{version}/STFU.dmg"
   name "STFU"
